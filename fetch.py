cf25d5d1e8882ea71ebfedb25a18aa6e9a6aa974
Athos Coimbra Ribeiro <ribeiro@fedoraproject.org>
2016-07-14 09:46:32 -0300

    Use functions for http requests

--- fetch.py
+++ fetch.py
@@ -16,21 +16,77 @@ LOG_FILE = 'retrieve.log'
 CAMARA_BASE_URL = 'http://www.camara.leg.br'
 API_ENTRY_POINT = '/SitCamaraWS'
 API_GET_CONGRESSMEN = '/Deputados.asmx/ObterDeputados'
-API_GET_PROPOSITIONS = '/Proposicoes.asmx/ListarProposicoes'
-API_GET_PROPOSITION_BY_ID = 'Proposicoes.asmx/ObterProposicaoPorID'
+API_GET_PROPOSALS = '/Proposicoes.asmx/ListarProposicoes'
+API_GET_PROPOSAL_BY_ID = '/Proposicoes.asmx/ObterProposicaoPorID'
 API_GET_SPEECHES = '/sessoesreunioes.asmx/ListarDiscursosPlenario'
 
 logging.basicConfig(filename=LOG_FILE, level=logging.WARNING)
 
+
+def fetch_all_congressmen():
+    """
+    returns a XML with all congressmen data
+    """
+    try:
+        response = requests.get(CAMARA_BASE_URL + API_ENTRY_POINT +
+                                API_GET_CONGRESSMEN)
+    except Exception:
+        logging.warning('Could not GET data')
+    return response.content
+
+
+# set sigla=PL for projetos de lei
+def fetch_cm_proposals(cm_name, cm_party, cm_state, start_date, end_date):
+    payload = {'sigla': 'PL',
+               'numero': '',
+               'ano': '',
+               'datApresentacaoIni': start_date,
+               'datApresentacaoFim': end_date,
+               'parteNomeAutor': cm_name,
+               'idTipoAutor': '',
+               'siglaPartidoAutor': cm_party,
+               'siglaUFAutor': cm_state,
+               'generoAutor': '',
+               'codEstado': '',
+               'codOrgaoEstado': '',
+               'emTramitacao': ''}
+    try:
+        response = requests.get(CAMARA_BASE_URL + API_ENTRY_POINT +
+                                API_GET_PROPOSALS, params=payload)
+    except Exception:
+        logging.warning('Could not GET data')
+    return response.content
+
+
+def fetch_proposal_by_id(proposal_id):
+    payload = {'idProp': proposal_id}
+    try:
+        response = requests.get(CAMARA_BASE_URL + API_ENTRY_POINT +
+                                API_GET_PROPOSAL_BY_ID, params=payload)
+    except Exception:
+        logging.warning('Could not GET data')
+    return response.content
+
+
+def fetch_cm_speeches(cm_name, cm_party, cm_state, start_date, end_date):
+    payload = {'dataIni': start_date,
+               'dataFim': end_date,
+               'codigoSessao': '',
+               'parteNomeParlamentar': cm_name,
+               'siglaPartido': cm_party,
+               'siglaUF': cm_state}
+    try:
+        response = requests.get(CAMARA_BASE_URL + API_ENTRY_POINT +
+                                API_GET_SPEECHES, params=payload)
+    except Exception:
+        logging.warning('Could not GET data')
+    return response.content
+
 # Get all congressmen information
-try:
-    cm = requests.get('http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados')
-except Exception:
-    logging.warning('Could not GET data')
 
 deputados = dict()
 try:
-    obj = untangle.parse(cm.content)
+    obj = untangle.parse(fetch_all_congressmen())
     for dep in obj.deputados.deputado:
         deputados[dep.nomeParlamentar.cdata] = dict()
         deputados[dep.nomeParlamentar.cdata]['nome'] = dep.nome.cdata
@@ -39,6 +95,8 @@ try:
         deputados[dep.nomeParlamentar.cdata]['partido'] = dep.partido.cdata
         deputados[dep.nomeParlamentar.cdata]['fone'] = dep.fone.cdata
         deputados[dep.nomeParlamentar.cdata]['email'] = dep.email.cdata
+        # FIXME: DEBUG
+        #print dep.nomeParlamentar.cdata
 except IndexError as e:
     logging.warning(e)
 
@@ -46,46 +104,36 @@ except IndexError as e:
 for dep in deputados:
     try:
         # set sigla=PL for projetos de lei
-        propositions = requests.get('http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?sigla=&numero=&ano=&datApresentacaoIni=18/07/2015&datApresentacaoFim=12/07/2016&parteNomeAutor=%s&idTipoAutor=&siglaPartidoAutor=%s&siglaUFAutor=%s&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao=' % (dep, deputados[dep]['partido'], deputados[dep]['uf']))
-    except Exception:
-        logging.warning('Could not GET data')
-    try:
-        obj = untangle.parse(propositions.content)
+        obj = untangle.parse(fetch_cm_proposals(dep, deputados[dep]['partido'], deputados[dep]['uf'], '05/07/2016', '12/07/2016'))
         for prop in obj.proposicoes.proposicao:
             deputados[dep]['proposicoes'] = dict()
             deputados[dep]['proposicoes'][prop.id.cdata] = dict()
 
             # Get proposal keywords and text
             try:
-                detailed_prop = requests.get('http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?idProp=%s' % (prop.id.cdata))
-            except Exception:
-                logging.warning('Could not GET data')
-            try:
-                obj2 = untangle.parse(detailed_prop.content)
+                obj2 = untangle.parse(fetch_proposal_by_id(prop.id.cdata))
                 deputados[dep]['proposicoes'][prop.id.cdata]['key words'] = obj2.proposicao.Indexacao.cdata
+                # FIXME: DEBUG 2
+                # print obj2.proposicao.Indexacao.cdata
 
             except IndexError as e:
                 logging.warning(e)
 
-    except IndexError as e:
-        logging.warning("'%s'\n\tin:\n%s\n\tfor: %s" % (e, propositions.content, dep))
+    except Exception as e:
+        logging.warning("'%s'\n\tfor: %s" % (e, dep))
 
 # Get all speeches for each congressman
 for dep in deputados:
     try:
-        speeches = requests.get('http://www.camara.leg.br/sitcamaraws/sessoesreunioes.asmx/ListarDiscursosPlenario?dataIni=18/07/2015&dataFim=12/07/2016&codigoSessao=&parteNomeParlamentar=%s&siglaPartido=%s&siglaUF=%s' % (dep, deputados[dep]['partido'], deputados[dep]['uf']))
-    except Exception:
-        logging.warning('Could not GET data')
-    try:
-        obj = untangle.parse(speeches.content)
+        obj = untangle.parse(fetch_cm_speeches(dep, deputados[dep]['partido'], deputados[dep]['uf'], '05/07/2016', '12/07/2016'))
         for session in obj.sessoesDiscursos.sessao:
             for phase in session.fasesSessao.faseSessao:
                 for speech in phase.discursos.discurso:
                     deputados[dep]['discursos'] = []
                     deputados[dep]['discursos'].append(speech.txtIndexacao.cdata)
 
-    except IndexError as e:
+    except Exception as e:
         logging.warning(e)
 
 with open('data.json', 'w') as outfile:
-    json.dumps(deputados, outfile)
+    json.dump(deputados, outfile, ensure_ascii=False)
